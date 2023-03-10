cd5f822343fc5c89a5561f81f5ecee505a89523a
220005874 <kindred0425@gmail.com>
2023-03-10 16:39:00 +0000

    modify server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,1 +1,13 @@
-console.log('H')
+const express = require('express')
+const app = express()
+
+/*
+ * GET: request and response
+ * https://www.youtube.com/watch?v=SccSCuHhOw0
+ */
+app.get('/',(req,res) => {
+    console.log("Here");
+    res.sendStatus(500) //err on our server
+    res.send("Hi")
+})
+app.listen(3000)
