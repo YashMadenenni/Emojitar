e6de973e1f2c0de423fbc75904149d89c3329a84
220005874 <kindred0425@gmail.com>
2023-03-10 10:47:30 +0000

    change the tab from lib to manuel setting

---
+++ src/client/client.js
@@ -0,0 +1,17 @@
+function openPage(pageName) {
+  var tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  var tablinks = document.getElementsByClassName("tablink");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(pageName).style.display = "block";
+  event.currentTarget.classList.add("active");
+}
+
+
+window.onload = function() {
+  document.getElementById("defaultActive").click();
+}
