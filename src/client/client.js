55cc6903ed67706f06845c16459a5638c16bb697
220005874 <kindred0425@gmail.com>
2023-03-12 04:20:09 +0000

    add CANVAS feature

--- src/client/client.js
+++ src/client/client.js
@@ -3,6 +3,11 @@ const faceImages = [];
 const eyesImages = [];
 const mouthImages = [];
 const hairImages = [];
+
+let faceImage = new Image();
+let eyesImage = new Image();
+let mouthImage = new Image();
+let hairImage = new Image();
 /**
  * Function Purpose: load the page while window onload
  * 1. getAllFacialComponent(): fetch facial components from the server
@@ -65,17 +70,21 @@ function createFacialComponentButton(elementID, images) {
   html.innerHTML= '';
 
   images.forEach(image => {
-    let htmlSegment = `<div class="button-wrapper">
+    let htmlSegment = `<div class="button-wrapper" onclick=canvas("${image.type}","${image.filename}")>
                           <div id="button">
                             <span class="tooltiptext">${image.description}</span>
                             <img id ="${image.filename}" src="${image.url}" alt="not found">
                           </div>
-                        </div>`;
+                      </div>`;
     html.innerHTML += htmlSegment;
   });
 }
 /**
- * Function: to create facial componenets girds: face/mouth/eyes/hair
+ * Function: to create facial componenets girds: 
+ * 1.face
+ * 2.mouth
+ * 3.eyes
+ * 4.hair
  */
 function createAllFacialComponentButton() {
   createFacialComponentButton("faceScroll",faceImages);
@@ -84,11 +93,53 @@ function createAllFacialComponentButton() {
   createFacialComponentButton("hairScroll",hairImages);
 }
 /**
+ * https://stackoverflow.com/questions/14742381/combining-multiple-images-to-one-on-html5-canvas
+ */
+function drawEmojitar() {
+  let canvas = document.querySelector(".emoji-canvas");
+  let context = canvas.getContext("2d");
+
+  canvas.width = 300;
+  
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.drawImage(faceImage, 0, 50, canvas.width, canvas.width);
+  context.drawImage(eyesImage, 0, 50, canvas.width, canvas.width);
+  context.drawImage(mouthImage, 0, 50, canvas.width, canvas.width);
+  context.drawImage(hairImage, 0, 50, canvas.width, canvas.width);
+}
+/**
+ * Function: to set the emojitar canvas
+ * @param {*} componentType face/eyes/mouth/hair
+ * @param {*} imageFilename 
+ */
+function canvas(componentType, imageFilename) {
+  let path = '/emojis/'
+  path += imageFilename;
+
+  switch (componentType) {
+    case "face":
+      faceImage.src = path;
+      faceImage.onload = drawEmojitar();
+      break;
+    case "eyes":
+      eyesImage.src = path;
+      eyesImage.onload = drawEmojitar();
+      break;
+    case "mouth":
+      mouthImage.src = path;
+      mouthImage.onload = drawEmojitar();
+      break;
+    case "hair":
+      hairImage.src = path;
+      hairImage.onload = drawEmojitar();
+      break;
+    }
+}
+/**
  * Call the functions while loading/refreshing
  */
 window.onload = function() {
   loadPage();
-
 }
 
 // example for sending Post emoji request
